94a22ead5dd8397ee1188709167670a456e008fd
ekaterinaivanc <kataivancova7@gmail.com>
2025-11-24 22:52:50 +0300

    Add struct Vector

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
+#include <stdexcept>
 
 struct Planar
 {
-  virtual int x() const;
-  virtual int y() const;
-  virtual int abc_sqr() const = 0;
+  virtual int x() const = 0;
+  virtual int y() const = 0;
+  virtual int abs_sqr() const = 0;
   virtual ~Planar() = default;
 };
 
-Planar * make(std::istream& is);
+//Planar * make(std::istream& is);
 Planar * make(size_t pl);
 void draw(Planar * pl);
 void free_planars(Planar ** pls, size_t k);
@@ -21,6 +22,19 @@ struct Point: Planar
   virtual int abs_sqr() const;   
   Point(int xx, int yy);
   virtual ~Point() = default;
+
+  private:
+    int data[2];
+};
+
+struct Vector: Planar 
+{
+  virtual int x() const;
+  virtual int y() const;
+  virtual int abs_sqr() const;   
+  Vector(int xx, int yy);
+  Vector(const Point & t1, const Point & t2);
+  virtual ~Vector() = default;
 
   private:
     int data[2];
@@ -45,14 +59,13 @@ int main()
   draw(mostleft(pls, k));
   free_planars(pls, k);
   //pls[k++] = make(std::cin);
-  //Planar * left = mostleft(pls, k);
-  //draw(left);
-  //free_planars(pls, k);
+  Planar * left = mostleft(pls, k);
+  draw(left);
 }
 
 Point::Point(int xx, int yy):
   Planar(),
-  data{xx, yy} //можно в фигурных
+  data{xx, yy}
 {}
 
 int Point::x() const
@@ -70,6 +83,29 @@ int Point::abs_sqr() const
   return x() * x() + y() * y();  
 }
 
+Vector::Vector(const Point & t1, const Point & t2):
+  data{t2.x() - t1.x(), t2.y() - t1.y()}
+  {}
+
+Vector::Vector(int xx, int yy):
+  data{xx, yy}
+  {}
+
+int Vector::x() const
+{
+  return data[0];
+}
+
+int Vector::y() const
+{
+  return data[1];  
+}
+
+int Vector::abs_sqr() const
+{
+  return x() * x() + y() * y();  
+}
+
 Planar * make(size_t id)
 {
   Planar * r = nullptr;
@@ -77,6 +113,9 @@ Planar * make(size_t id)
   {
     case 0:
       r = new Point(0,0);
+      break;
+    case 1:
+      r = new Vector(1,1);
       break;
     default:
       throw std::logic_error("bad id");
