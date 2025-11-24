3825495b61ba6722ce538c0a859b5dc86a32cf72
ekaterinaivanc <kataivancova7@gmail.com>
2025-11-24 12:48:25 +0300

    Add struct Planar

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 
+struct Planar
+{
+  virtual int x() const;
+  virtual int y() const;
+  virtual int abc_sqr() const = 0;
+  virtual ~Planar() = default;
+};
+
+Planar * make(std::istream& is);
+Planar * mostleft(Planar ** pls, size_t k);
+
 int main()
 {
+  Planar * pls[10] = {};
+  size_t k = 0;
+  //pls[k++] = make(std::cin);
+  //Planar * left = mostleft(pls, k);
+  //draw(left);
+  //free_planars(pls, k);
 }
